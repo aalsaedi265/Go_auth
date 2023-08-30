515e54bc8784e2f53beb21917207117617a4cf11
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-08-29 20:45:04 -0500

    connection established from db to code on vscode

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 
 package main
 
-import "github.com/gofiber/fiber/v2"
+import( 
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+    "github.com/denisenkom/go-mssqldb"
+)
 
 func main(){
+	dbString := "sqlserver://ADANOS\\aalsa@Adanos\\SQLEXPRESS?database=Go_auth"
+	_, err := gorm.Open("mssql", dbString)
+	if err != nil {
+        panic("could not connect to database")
+	}
+
 
 	app := fiber.New()
 	app.Get("/", func(c*fiber.Ctx) error{
