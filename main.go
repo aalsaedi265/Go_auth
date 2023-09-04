275efbcf85b47204630cd97b4dab2a180da544da
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-09-04 12:40:08 -0500

    json

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 
-package main
+package main 
 
-import( 
-	"fmt"
-	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/sqlserver"
-	"gorm.io/gorm"
-)
+import
+"fmt"
 
 func main() {
-	dsn :=  "server=localhost;database=Go_auth;integrated security=SSPI;"
-	_, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		panic("could not connect to the database")
-	}
-
-	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, world!")
-	})
-
-	app.Listen(":8000")
+	fmt.Println("the world")
 }
