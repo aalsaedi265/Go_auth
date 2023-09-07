eead149ad2a981d5356064b4b2d45ca5e1d0a490
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-09-07 10:02:22 -0500

    handlers in progess

--- api.go
+++ api.go
@@ -1,6 +1,31 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func WriteJson(w http.ResponseWriter, status int, v any) error{
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request){
+		if err := f(w, r); err != nil {
+			WriteJson(w,http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
 
 type APIServer struct {
 	listenAddr string
