c837b77e63facc86e5e7ca41902511dbb9a22608
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-09-08 14:13:37 -0500

    upadting the hanles

--- api.go
+++ api.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 )
@@ -40,15 +42,33 @@ func NewAPIServer(listenAddr string) *APIServer {
 func( s *APIServer) Run(){
 	router := mux.NewRouter()
 
-	router.HandleFunc("/account", s.handleAccount)
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount))
+
+	log.Println("JSON API server running on port: ", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
 }
 
 func(s *APIServer)handleAccount( w http.ResponseWriter, r *http.Request) error{
-	return nil
+	if r.Method == "GET"{
+		return s.handleAccount(w, r)
+	}
+	if r.Method == "POST"{
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE"{
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("Method forbidden: %s", r.Method) 
 }
 
 func(s *APIServer)handleGetAccount( w http.ResponseWriter, r *http.Request) error{
-	return nil
+	
+	account := NewAccount("Funny,", "Valintine")
+
+	return WriteJson(w, http.StatusOK, account)
 }
 
 func(s *APIServer)handleCreateAccount( w http.ResponseWriter, r *http.Request) error{
@@ -56,5 +76,9 @@ func(s *APIServer)handleCreateAccount( w http.ResponseWriter, r *http.Request) e
 }
 
 func(s *APIServer)handleTransfer( w http.ResponseWriter, r *http.Request) error{
+	return nil
+}
+
+func(s *APIServer)handleDeleteAccount( w http.ResponseWriter, r *http.Request) error{
 	return nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 
 package main 
 
-import
-"fmt"
+
 
 func main() {
-	fmt.Println("the world")
+	server := NewAPIServer(":3000")
+	server.Run()
 }
